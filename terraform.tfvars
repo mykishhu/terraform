88f8342b08d62f5bcf97e01886857038a4c8c8da
Pattamatta CH N Kishore <mykishhu@gmail.com>
2023-01-25 11:18:24 +0530

    added terraform files

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+myregion = "us-east-1"
+myaccesskey = "AKIAU46JEHGBI2VUVAOA"
+mysecretkey = "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"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "myregion" {
+type = string
+description = "AWS Region to use"
+}
+
+variable "myaccesskey" {
+description = "AWs Access Key"
+}
+
+variable "mysecretkey" {
+description = "AWS Secret Key"
+}
